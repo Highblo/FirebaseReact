b2b4eb174ab0ea9e5a664346e90f8a2294859a33
siotan <nyaa.-525@outlook.jp>
2022-10-21 07:55:16 +0900

    db取得レンダリング

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import "./App.css";
 import db from "./firebase";
 import { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
 
 export const App = () => {
-  const [posts, setPosts] = useState();
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const querySnapshot = getDocs(collection(db, "posts"));
     querySnapshot.then((snapShot) => {
       setPosts(snapShot.docs.map((doc) => doc.data()));
-      console.log(posts);
+    });
+
+    /* リアルタイムで取得 */
+    onSnapshot(collection(db, "posts"), (post) => {
+      setPosts(post.docs.map((doc) => doc.data()));
     });
   }, []);
 
   return (
     <div className="App">
+      <div>
+        {posts.map((post) => (
+          <div key={post.title}>
+            <h1>{post.title}</h1>
+            <p>{post.text}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
